d2a72475f31d517ba1653e7517126a39521461ff
Gland0rf <ndkogler@icloud.com>
2026-05-29 18:57:44 +0200

    Changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+cd /home/nico/Documents/Memora
+
 git fetch origin
 git reset --hard origin/feature/hosting
+git clean -fd
 
-bash ~/deploy-memory-frontend.sh
+bash ~/deploy-memora-frontend.sh
+bash ~/deploy-memora-backend.sh
